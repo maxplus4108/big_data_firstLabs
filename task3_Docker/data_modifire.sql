9ea79d2c5b2d7755aba445fa5e426c5fc3675eb6
maxplus4108 <max.plus4108@gmail.com>
2026-02-26 10:17:01 +0500

    Задание 3

--- task3_Docker/data_modifire.sql
+++ task3_Docker/data_modifire.sql
@@ -7,10 +7,28 @@ SET s_all_avg = REPLACE(s_all_avg, ',', '.')
 WHERE s_all_avg LIKE '%,%';
 
 
+UPDATE user_logs 
+SET 
+    s_course_viewed_avg = REPLACE(s_course_viewed_avg::text, ',', '.'),
+    s_q_attempt_viewed_avg = REPLACE(s_q_attempt_viewed_avg::text, ',', '.'),
+    s_a_course_module_viewed_avg = REPLACE(s_a_course_module_viewed_avg::text, ',', '.'),
+    s_a_submission_status_viewed_avg = REPLACE(s_a_submission_status_viewed_avg::text, ',', '.')
+WHERE 
+    s_course_viewed_avg::text LIKE '%,%' OR 
+    s_q_attempt_viewed_avg::text LIKE '%,%' OR 
+    s_a_course_module_viewed_avg::text LIKE '%,%' OR 
+    s_a_submission_status_viewed_avg::text LIKE '%,%';
+
+
 -- 3. Изменение типа данных колонки со строкового на числовой (REAL)
 ALTER TABLE user_logs 
 ALTER COLUMN s_all_avg TYPE REAL USING s_all_avg::REAL;
 
+ALTER TABLE user_logs 
+    ALTER COLUMN s_course_viewed_avg TYPE REAL USING NULLIF(s_course_viewed_avg::text, '')::REAL,
+    ALTER COLUMN s_q_attempt_viewed_avg TYPE REAL USING NULLIF(s_q_attempt_viewed_avg::text, '')::REAL,
+    ALTER COLUMN s_a_course_module_viewed_avg TYPE REAL USING NULLIF(s_a_course_module_viewed_avg::text, '')::REAL,
+    ALTER COLUMN s_a_submission_status_viewed_avg TYPE REAL USING NULLIF(s_a_submission_status_viewed_avg::text, '')::REAL;
 
 -- 4. Расчет среднего значения по очищенной колонке
 SELECT AVG(s_all_avg) AS total_average_activity 
